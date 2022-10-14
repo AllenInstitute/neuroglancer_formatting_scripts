10683ec6370afb4e53c2c39d2316d1fc9a848018
danielsf <scott.daniel@alleninstitute.org>
2022-10-14 15:54:08 -0700

    specify multiple genes and colors

--- create_url.py
+++ create_url.py
@@ -1,6 +1,16 @@
 import argparse
 import json
 
+def get_color_lookup():
+
+    return {"red": (1, 0, 0),
+            "green": (0, 1, 0),
+            "blue": (0, 0, 1),
+            "orange": (252/255, 186/255, 3/255),
+            "cyan": (3/255, 252/255, 227/255),
+            "magenta": (252/255, 3/255, 252/255)}
+
+
 def get_pct_to_char():
     result = {
         "%20": " ",
@@ -82,14 +92,40 @@ def get_mfish(
         mfish_gene,
         mfish_color):
 
+    rgb_color = get_color_lookup()[mfish_color]
     result = dict()
     result["type"] = "image"
     result["source"] = f"zarr://s3://{mfish_bucket}/{mfish_gene}",
-    result["name"] = mfish_gene
+    result["name"] = f"{mfish_gene} ({mfish_color})"
     result["blend"] = "additive"
-    result["shader"] = get_shader_code(mfish_color)
+    result["shader"] = get_shader_code(rgb_color)
+    result["opacity"] = 1
     return result
 
+
+def get_gene_layers(
+        mfish_bucket,
+        gene_list,
+        color_list):
+
+    with open("mouse1_gene_list.json", "rb") as in_file:
+        legal_genes = set(json.load(in_file))
+
+    if len(gene_list) != len(color_list):
+        raise RuntimeError(
+             f"{len(gene_list)} genes but "
+             f"{len(color_list)} colors")
+
+    layers = []
+    for gene, color in zip(gene_list, color_list):
+        if gene not in legal_genes:
+            raise RuntimeError(
+                f"{gene} is not a legal gene")
+        layers.append(get_mfish(
+                          mfish_bucket=mfish_bucket,
+                          mfish_gene=gene,
+                          mfish_color=color))
+    return layers
 
 def main():
 
@@ -97,14 +133,36 @@ def main():
     parser.add_argument('--segmentation_bucket',
                         type=str,
                         default='mouse1-atlas-prototype')
+
     parser.add_argument('--segmentation_name',
                         type=str,
                         default='segmentation')
+
     parser.add_argument('--mfish_bucket',
                         type=str,
                         default='mouse1-mfish-prototype')
 
+    parser.add_argument('--genes',
+                        type=str,
+                        nargs='+',
+                        default=None)
+
+    parser.add_argument('--colors',
+                        type=str,
+                        nargs='+',
+                        default=None)
+
     args = parser.parse_args()
+
+    if isinstance(args.genes, str):
+        genes = [args.genes]
+    else:
+        genes = args.genes
+
+    if isinstance(args.colors, str):
+        colors = [args.colors]
+    else:
+        colors = args.colors
 
     url = get_base_url()
 
@@ -112,17 +170,12 @@ def main():
                             segmentation_bucket=args.segmentation_bucket,
                             segmentation_name="segmentation")
 
-    mfish_layer = get_mfish(
+    gene_layers = get_gene_layers(
                     mfish_bucket=args.mfish_bucket,
-                    mfish_gene='Prox1',
-                    mfish_color=(1, 0, 0))
+                    gene_list=genes,
+                    color_list=colors)
 
-    mfish_layer2 = get_mfish(
-                    mfish_bucket=args.mfish_bucket,
-                    mfish_gene='Cpne9',
-                    mfish_color=(0, 1, 0))
-
-    layers = {"layers": [segmentation_layer, mfish_layer, mfish_layer2]}
+    layers = {"layers": [segmentation_layer] + gene_layers}
     layers["selectedLayer"] = {"visible": True, "layer": "new layer"}
     layers["layout"] = "4panel"
     url = f"{url}{json_to_url(json.dumps(layers))}"
