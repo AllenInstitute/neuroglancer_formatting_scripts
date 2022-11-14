7d6c859bc3f91cd691ade20d72db58e0fa91380d
danielsf <scott.daniel@alleninstitute.org>
2022-11-14 12:19:06 -0800

    do not transpose flatmap average template

--- convert_flatmap_to_ome_zarr.py
+++ convert_flatmap_to_ome_zarr.py
@@ -1,7 +1,8 @@
 import pathlib
 import argparse
 from neuroglancer_interface.utils.data_utils import (
-    write_nii_file_list_to_ome_zarr)
+    write_nii_file_list_to_ome_zarr,
+    write_nii_to_group)
 
 
 def main():
@@ -23,7 +24,7 @@ def main():
     data_to_group = dict()
     avg_path = input_dir /"average_template_10_RightDV_flatmap.nrrd"
     assert avg_path.is_file()
-    data_to_group[avg_path] = "avg_template"
+    #data_to_group[avg_path] = "avg_template"
 
     layer_dir = input_dir / "projections/flatmaps/layers"
     assert layer_dir.is_dir()
@@ -40,12 +41,19 @@ def main():
         file_path_list.append(pth)
         group_name_list.append(data_to_group[pth])
 
-    write_nii_file_list_to_ome_zarr(
+    root_group = write_nii_file_list_to_ome_zarr(
         file_path_list=file_path_list,
         group_name_list=group_name_list,
         output_dir=output_dir,
         downscale=args.downscale,
         clobber=args.clobber)
+
+    write_nii_to_group(
+        root_group=root_group,
+        group_name="avg_template",
+        nii_file_path=avg_path,
+        downscale=2,
+        transpose=False)
 
 
 if __name__ == "__main__":
