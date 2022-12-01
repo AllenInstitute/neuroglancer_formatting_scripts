dcbb1d8633dc4c4bd08c51aa475faa4665df333e
danielsf <scott.daniel@alleninstitute.org>
2022-12-01 15:50:20 -0800

    save masks for validation purposes

--- src/neuroglancer_interface/classes/metadata_collectors.py
+++ src/neuroglancer_interface/classes/metadata_collectors.py
@@ -37,7 +37,8 @@ class CellTypeMetadataCollector(object):
             if self.masks[k] is not None:
                 local_masks[k] = dict()
                 for el in self.masks[k]:
-                    local_masks[k][el] = self.masks[k][el]['path']
+                    local_masks[k][el] = {'path': self.masks[k][el]['path'],
+                                          'mask': [[int(v[ii]) for ii in range(len(v))] for v in self.masks[k][el]['mask']]}
 
         metadata['masks'] = local_masks
 
