68c36573d62a3bd26625e95ed2bab8c24079a865
danielsf <scott.daniel@alleninstitute.org>
2022-12-05 15:36:03 -0800

    attempt to finish create_tissuecyte_html

--- create_tissuectye_html.py
+++ create_tissuectye_html.py
@@ -1,9 +1,15 @@
+import pathlib
+import json
+import argparse
+
 from neuroglancer_interface.utils.url_utils import (
-    url_to_json,
     get_final_url,
     get_heatmap_image_layer,
     get_template_layer,
     get_segmentation_layer)
+
+from neuroglancer_interface.utils.html_utils import (
+    write_basic_table)
 
 
 def create_tissuecyte_url(
@@ -59,20 +65,94 @@ def create_tissuecyte_url(
     return url
 
 
+def create_tissuecyte_html(
+        dataset_dir,
+        s3_location,
+        table_title,
+        output_path):
+
+    dataset_dir = pathlib.Path(dataset_dir)
+
+    template_s3 = f"{s3_location}/{avg_template}"
+    segmentation_s3 = f"{s3_location}/{ccf_annotations}"
+
+    metadata_path = dataset_dir / "tissuecyte/metadata.json"
+    with open(metadata_path, "rb") as in_file:
+        metadata = json.load(metadata_path)
+
+    tissuecyte_dir = dataset_dir / "tissuecyte"
+    tissuecyte_dir_list = [n for n in tissuecyte_dir.iterdir()
+                           if n.is_dir()]
+
+    tissuecyte_dir_list.sort()
+    key_to_link = dict()
+    key_to_other_cols = dict()
+    key_order = []
+    for sub_dir in tissuecyte_dir_list:
+        series_id = sub_dir.name
+        tissuecyte_s3 = f"{s3_location}/{series_id}"
+        csv_path = sub_dir / "image_series_information.csv"
+        range_lookup = dict()
+        with open(csv_path, "r") as in_file:
+            header = in_file.readline().strip().split(',')
+            data = in_file.readline().strip().split(',')
+            assert len(header) == len(data)
+            for h, d in zip(header, data):
+                range_lookup[h] = d
+
+        red_max = float(range_lookup['red_upper'])
+        green_max = float(range_lookup['green_upper'])
+
+        this_metadata = metadata[f"{series_id}/green"]
+        x_mm = this_metadata["x_mm"]
+        y_mm = this_metadata["y_mm"]
+        z_mm = this_metadata["z_mm"]
+        url = create_tissuecyte_rul(
+            tissuecyte_s3=tissuecyte_s3,
+            segmentation_s3=segmentation_s3,
+            template_s3=template_s3,
+            x_mm=x_mm,
+            y_mm=y_mm,
+            z_mm=z_mm,
+            red_max=red_max,
+            green_max=green_max)
+
+        key_to_link[series_id] = url
+        key_order.append(series_id)
+        key_to_other_cols[series_id] = {'names': ['image_series_id'],
+                                        'values': [str(series_id)]}
+
+    metadata_lines = [
+        f"tissuecyte src: {tissuecyte_s3}",
+        f"template src: {template_s3}",
+        f"segmentation src: {segmentation_s3}"]
+
+    write_basic_table(
+        ouptut_path=output_path,
+        title=table_title,
+        key_to_link=key_to_link,
+        key_order=key_order,
+        div_name="tissuecyte",
+        search_by=['image_series_id'],
+        metadata_lines=metadata_lines)
+
+    print(f"wrote {output_path}")
+
+
 def main():
 
-    url = create_tissuecyte_url(
-        tissuecyte_s3="tissuecyte-visualizations/data/221205/tissuecyte/1122763685",
-        segmentation_s3="tissuecyte-visualizations/data/221205/ccf_annotations",
-        template_s3="tissuecyte-visualizations/data/221205/avg_template",
-        x_mm=0.01,
-        y_mm=0.01,
-        z_mm=0.01,
-        starting_position=None,
-        red_max=5000,
-        green_max=17000)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--s3_location", type=str, default=None)
+    parser.add_argument("--data_dir", type=str, default=None)
+    parser.add_argument("--output_path", type=str, default=None)
+    parser.add_argument("--table_title", type=str, default=None)
+    args = parser.parse_args()
 
-    print(url)
+    create_tissuecyte_html(
+        dataset_dir=args.data_dir,
+        s3_location=args.s3_location,
+        table_title=args.table_title,
+        output_path=args.output_path)
 
 
 if __name__ == "__main__":
