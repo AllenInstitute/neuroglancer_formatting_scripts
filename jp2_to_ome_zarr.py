31877a785f3fada0c904a84fd37cfe0f59f9e86d
danielsf <scott.daniel@alleninstitute.org>
2023-01-13 12:35:02 -0800

    update user-facing jp2-to-ome-zarr code

--- jp2_to_ome_zarr.py
+++ jp2_to_ome_zarr.py
@@ -1,20 +1,31 @@
 import argparse
 import pathlib
+import json
 
 from neuroglancer_interface.modules.jp2_to_ome_zarr import (
     convert_jp2_to_ome_zarr)
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--jp2_path', type=str, default=None)
+    parser.add_argument('--config_list_path', type=str, default=None)
     parser.add_argument('--output_dir', type=str, default=None)
     parser.add_argument('--clobber', default=False, action='store_true')
+    parser.add_argument('--nz_min', default=None, type=int)
+    parser.add_argument('--nz_max', default=None, type=int)
     args = parser.parse_args()
 
+    config_list = json.load(open(args.config_list_path, "rb"))
+
+    if args.nz_min is not None and args.nz_max is not None:
+        nz_slice = (args.nz_min, args.nz_max)
+    else:
+        nz_slice = None
+
     convert_jp2_to_ome_zarr(
-        jp2_path=pathlib.Path(args.jp2_path),
+        config_list=config_list,
         output_dir=pathlib.Path(args.output_dir),
-        clobber=args.clobber)
+        clobber=args.clobber,
+        nz_slice=nz_slice)
 
 if __name__ == "__main__":
     main()
--- src/neuroglancer_interface/modules/jp2_to_ome_zarr.py
+++ src/neuroglancer_interface/modules/jp2_to_ome_zarr.py
@@ -1,5 +1,11 @@
-import glymur
+from typing import List, Union, Any, Optional, Tuple
+
 import pathlib
+import numpy as np
+import dask.array
+
+from neuroglancer_interface.utils.jp2_utils import (
+    write_data_to_hdf5)
 
 from neuroglancer_interface.utils.data_utils import (
     write_array_to_group,
@@ -9,31 +15,65 @@ from neuroglancer_interface.classes.downscalers import XYScaler
 
 
 def convert_jp2_to_ome_zarr(
-        jp2_path: pathlib.Path,
+        config_list: List[dict],
         output_dir: pathlib.Path,
         clobber: bool = False,
         x_scale: float = 0.0003,
         y_scale: float = 0.0003,
-        z_scale: float = 1.0) -> None:
+        z_scale: float = 1.0,
+        tmp_dir: Union[str, pathlib.Path]=None,
+        nz_slice: Optional[Tuple[int, int]] = None) -> None:
     """
     Result is just written to the specified group.
     """
-
-    if not jp2_path.is_file():
-        raise RuntimeError(f"{jp2_path} is not a file")
 
     root_group = create_root_group(
                     output_dir=output_dir,
                     clobber=clobber)
 
-    data_src = glymur.Jp2k(jp2_path)
-    write_array_to_group(
-        arr=data_src[:, :, :],
-        group=root_group,
-        x_scale=x_scale,
-        y_scale=y_scale,
-        z_scale=z_scale,
-        downscale=2,
-        DownscalerClass=XYScaler,
-        downscale_cutoff=2048,
-        default_chunk=1024)
+    idx_list = np.array([r['specimen_tissue_index'] for r in config_list])
+    sorted_dex = np.argsort(idx_list)
+    config_list = [config_list[idx] for idx in sorted_dex]
+
+    if nz_slice is not None:
+        config_list = config_list[nz_slice[0]:nz_slice[1]]
+
+    h5_path = write_data_to_hdf5(
+                config_list=config_list,
+                tmp_dir=tmp_dir)
+
+    try:
+        _convert_jp2_to_ome_zarr(
+            h5_path=h5_pth,
+            root_group=root_group,
+            clobber=clobber,
+            x_scale=x_scale,
+            y_scale=y_scale,
+            z_scale=z_scale)
+    finally:
+        if h5_path.exists():
+            h5_path.unlink()
+
+def _convert_jp2_to_ome_zarr(
+        h5_path: pathlib.Path,
+        root_group: Any,
+        x_scale: float = 0.0003,
+        y_scale: float = 0.0003,
+        z_scale: float = 1.0) -> None:
+
+
+    with h5py.File(h5_path, 'r') as in_file:
+        for data_key in ('green', 'red'):
+            print(f"writing {data_key} channel")
+            data = in_file[data_key]
+
+        write_array_to_group(
+            arr=data,
+            group=root_group.create_group(data_key),
+            x_scale=x_scale,
+            y_scale=y_scale,
+            z_scale=z_scale,
+            downscale=2,
+            DownscalerClass=XYScaler,
+            downscale_cutoff=2048,
+            default_chunk=1024)
