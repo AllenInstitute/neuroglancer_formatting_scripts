f341c966ad7cfad696d0520681f556407a163936
danielsf <scott.daniel@alleninstitute.org>
2023-01-18 05:25:22 -0800

    add hdf5_to_ome_zarr.py

---
+++ hdf5_to_ome_arr.py
@@ -0,0 +1,35 @@
+from neuroglancer_interface.utils.data_utils import create_root_group
+from neuroglancer_interface.modules.jp2_to_ome_zarr import (
+   _convert_hdf5_to_ome_zarr)
+
+
+import pathlib
+import argparse
+
+def main():
+    #h5_path = pathlib.Path('/allen/aibs/technology/danielsf/highres_test_file.h5')
+    default_h5_path = '/local1/highres_test_file.h5'
+    default_output_dir = '/alen/ais/technology/danielsf/highres_compressed3'
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--h5_path', type=str, default=default_h5_path)
+    parser.add_argument('--output_dir', type=str, default=default_output_dir)
+    args = parser.parse_args()
+
+    h5_path = pathlib.Path(args.h5_path)
+    output_dir = args.output_dir
+
+    assert h5_path.is_file()
+
+    root_group = create_root_group(
+        output_dir=output_dir,
+        clobber=True)
+
+    _convert_hdf5_to_ome_zarr(
+        h5_path=h5_path,
+        root_group=root_group,
+        default_chunk=128,
+        downscale_cutoff=125)
+
+if __name__ == "__main__":
+    main()
