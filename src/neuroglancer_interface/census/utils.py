234e2b5b053c1da1bcf8c0576e5fc28047fc4512
danielsf <scott.daniel@alleninstitute.org>
2023-04-03 12:23:11 -0700

    add function to do census on one data array

--- src/neuroglancer_interface/census/utils.py
+++ src/neuroglancer_interface/census/utils.py
@@ -102,3 +102,68 @@ def get_mask_from_NIFTI(
     result['shape'] = arr.shape
     result['pixels'] = np.where(arr==1)
     return result
+
+
+def census_from_NIFTI_and_mask(
+        nifti_path,
+        mask_lookup):
+    """
+    Parameters
+    ----------
+    nifti_path:
+        Path to the NIFTI file containing the data (cell type density,
+        mfish expression, etc.)
+
+    mask_lookup:
+        Created by get_mask_lookup
+
+    Returns
+    -------
+    Dict
+        "structure" -> 
+            "counts": total counts of data in mask
+            "max_voxel": (z, y, x) coords of the maximum voxel
+            "per_plane": array indicating counts in the mask per plane
+    """
+    full_results = dict()
+
+    base_data = SimpleITK.GetArrayFromImage(
+        SimpleITK.ReadImage(nifti_path))
+
+    masked_data = np.zeros(base_data.shape, dtype=base_data.dtype)
+    negative_mask = np.ones(base_data.shape, dtype=bool)
+
+    structure_list = list(mask_lookup.keys())
+    structure_list.sort()
+
+    for structure_name in structure_list:
+        this_result = dict()
+        mask_config = mask_lookup[structure_name]
+        if mask_config['shape'] != base_data.shape:
+            raise RuntimeError(
+                f"structure {structure_name} has shape: "
+                f"{mask_config['shape']}\n"
+                f"{nifti_path} has shape {base_data.shape}")
+
+        # create a volume of data that is only non-zero
+        # inside the mask
+        negative_mask[:, :, :] = True
+        masked_data[:, :, :] = base_data[:, :, :]
+        negative_mask[mask_config['pixels']] = False
+        masked_data[negative_mask] = 0
+
+        # find max voxel
+        max_voxel_idx = np.argmax(masked_data)
+        max_voxel = np.unravel_index(max_voxel_idx, base_data.shape)
+        this_result['max_voxel'] = max_voxel
+
+        # find per plane sums
+        per_plane = np.sum(masked_data, axis=(1, 2))
+        this_result['per_plane'] = per_plane
+
+        # find total counts
+        this_result['counts'] = masked_data.sum()
+
+        full_results[structure_name] = this_result
+
+    return full_results
--- tests/census/test_census_utils.py
+++ tests/census/test_census_utils.py
@@ -10,7 +10,8 @@ from neuroglancer_interface.utils.utils import (
 
 from neuroglancer_interface.census.utils import (
     get_mask_from_NIFTI,
-    get_mask_lookup)
+    get_mask_lookup,
+    census_from_NIFTI_and_mask)
 
 
 
@@ -63,21 +64,25 @@ def test_get_mask_from_NIFTI(
 
 
 @pytest.fixture
-def mask_list_fixture():
+def shape_fixture():
+    return (22, 47, 53)
+
+@pytest.fixture
+def mask_list_fixture(shape_fixture):
     """
     List of dicts:
         "structure": the name of the structure
         "mask": array that is the structure mask
     """
-    rng = np.random.default_rng()
+    rng = np.random.default_rng(553322)
+    n_voxels = shape_fixture[0]*shape_fixture[1]*shape_fixture[2]
     result = []
     for ii in range(17):
         name = f"structure_{ii}"
-        shape = tuple(rng.integers(10, 30, size=3))
-        mask = np.zeros(shape[0]*shape[1]*shape[2], dtype=int)
-        chosen = rng.choice(len(mask), rng.integers(10, 50))
+        mask = np.zeros(n_voxels, dtype=int)
+        chosen = rng.choice(len(mask), rng.integers(10, n_voxels//2))
         mask[chosen] = 1
-        mask = mask.reshape(shape)
+        mask = mask.reshape(shape_fixture)
         result.append({'structure': name, 'mask': mask})
     return result
 
@@ -136,3 +141,88 @@ def test_get_mask_lookup(
             np.testing.assert_array_equal(
                 this_actual['pixels'][ii],
                 expected_result['pixels'][ii])
+
+
+@pytest.fixture
+def data_array_fixture(
+        shape_fixture):
+    """
+    Array of simulated data
+    """
+    rng = np.random.default_rng(887123)
+    return rng.random(shape_fixture)
+
+
+@pytest.fixture
+def nifti_data_fixture(
+        data_array_fixture,
+        tmp_path_factory):
+    """
+    Path to a NIFTI file containing example data
+    """
+    tmp_dir = pathlib.Path(
+        tmp_path_factory.mktemp('with_nifti_data'))
+    tmp_path = mkstemp_clean(
+        dir=tmp_dir,
+        suffix='.nii')
+
+    img = SimpleITK.GetImageFromArray(data_array_fixture)
+    SimpleITK.WriteImage(
+        image=img,
+        fileName=tmp_path)
+
+    yield pathlib.Path(tmp_path)
+
+    _clean_up(tmp_path)
+
+
+def test_census_from_nifti_and_mask(
+        data_array_fixture,
+        nifti_data_fixture,
+        mask_list_fixture,
+        structure_list_nifti_fixture):
+
+    mask_lookup = get_mask_lookup(
+        nifti_config_list=structure_list_nifti_fixture,
+        n_processors=3)
+
+    actual_census = census_from_NIFTI_and_mask(
+        nifti_path=nifti_data_fixture,
+        mask_lookup=mask_lookup)
+
+    # compare actual results to brute force results
+    for expected_mask, expected_config in zip(mask_list_fixture,
+                                              structure_list_nifti_fixture):
+        actual = actual_census[expected_config['structure']]
+        mask = expected_mask['mask'].astype(bool)
+        expected_counts = data_array_fixture[mask].sum()
+        np.testing.assert_allclose(actual['counts'],
+                                   expected_counts,
+                                   rtol=1.0e-6,
+                                   atol=0.0)
+
+        max_voxel = None
+        max_val = None
+        pixels = mask_lookup[expected_config['structure']]['pixels']
+        for (ii, jj, kk) in zip(pixels[0], pixels[1], pixels[2]):
+            vv = data_array_fixture[ii, jj, kk]
+            if max_val is None or vv > max_val:
+                max_val = vv
+                max_voxel = (ii, jj, kk)
+        np.testing.assert_array_equal(max_voxel, actual['max_voxel'])
+
+        pixels_ii = pixels[0]
+        pixels_jj = pixels[1]
+        pixels_kk = pixels[2]
+        per_plane = np.zeros(data_array_fixture.shape[0], dtype=float)
+        for unq_slice in np.unique(pixels_ii):
+            valid = (pixels_ii==unq_slice)
+            this = data_array_fixture[pixels_ii[valid],
+                                      pixels_jj[valid],
+                                      pixels_kk[valid]].sum()
+            per_plane[unq_slice] = this
+        np.testing.assert_allclose(
+            per_plane,
+            actual['per_plane'],
+            rtol=1.0e-6,
+            atol=0.0)
