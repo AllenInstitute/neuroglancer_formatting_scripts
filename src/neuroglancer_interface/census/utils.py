100562e0079e5e407a7d848c878095fca1096dbc
danielsf <scott.daniel@alleninstitute.org>
2023-04-03 11:30:14 -0700

    add method to read all structure masks

--- src/neuroglancer_interface/census/utils.py
+++ src/neuroglancer_interface/census/utils.py
@@ -1,6 +1,80 @@
+import multiprocessing
 import numpy as np
 import pathlib
 import SimpleITK
+
+
+def get_mask_lookup(
+        nifti_config_list,
+        n_processors):
+    """
+    Parameters
+    -----------
+    nifti_config_list:
+        List of configs like
+            {'path': path/to/nifti/file
+             'structure': name_of_structure}
+
+    n_processors:
+        Number of independent workers to spin up
+
+    Returns
+    -------
+    Dict
+        maps name_to_structure to a dict for each mask containing
+        'path'
+        'shape'
+        'pixels'
+        (see get_mask_from_NIFTI for documentation)
+    """
+    # make sure all structure tags are unique
+    structure_tag_names = set()
+    for config in nifti_config_list:
+        if config['structure'] in structure_tag_names:
+            raise RuntimeError(
+                f"structure name {config['structure']} occurs "
+                f"more than once")
+        structure_tag_names.add(config['structure'])
+
+    mgr = multiprocessing.Manager()
+    output_dict = mgr.dict()
+    output_lock = mgr.Lock()
+    process_list = []
+
+    n_per = max(1, len(nifti_config_list)//n_processors)
+    for i_batch in range(n_processors):
+        i0 = i_batch*n_per
+        if i_batch == n_processors-1:
+            i1 = len(nifti_config_list)
+        else:
+            i1 = i0 + n_per
+        batch = nifti_config_list[i0:i1]
+        p = multiprocessing.Process(
+                target=_get_mask_lookup_worker,
+                kwargs={
+                    'nifti_config_list': batch,
+                    'output_dict': output_dict,
+                    'output_lock': output_lock})
+        p.start()
+        process_list.append(p)
+    for p in process_list:
+        p.join()
+    return dict(output_dict)
+
+
+def _get_mask_lookup_worker(
+        nifti_config_list,
+        output_dict,
+        output_lock):
+
+    these = dict()
+    for config in nifti_config_list:
+        mask = get_mask_from_NIFTI(
+            nifti_path=config['path'])
+        these[config['structure']] = mask
+
+    with output_lock:
+        output_dict.update(these)
 
 
 def get_mask_from_NIFTI(
--- tests/census/test_census_utils.py
+++ tests/census/test_census_utils.py
@@ -9,7 +9,8 @@ from neuroglancer_interface.utils.utils import (
     mkstemp_clean)
 
 from neuroglancer_interface.census.utils import (
-    get_mask_from_NIFTI)
+    get_mask_from_NIFTI,
+    get_mask_lookup)
 
 
 
@@ -59,3 +60,79 @@ def test_get_mask_from_NIFTI(
     for ii in range(3):
         np.testing.assert_array_equal(
             expected[ii], actual['pixels'][ii])
+
+
+@pytest.fixture
+def mask_list_fixture():
+    """
+    List of dicts:
+        "structure": the name of the structure
+        "mask": array that is the structure mask
+    """
+    rng = np.random.default_rng()
+    result = []
+    for ii in range(17):
+        name = f"structure_{ii}"
+        shape = tuple(rng.integers(10, 30, size=3))
+        mask = np.zeros(shape[0]*shape[1]*shape[2], dtype=int)
+        chosen = rng.choice(len(mask), rng.integers(10, 50))
+        mask[chosen] = 1
+        mask = mask.reshape(shape)
+        result.append({'structure': name, 'mask': mask})
+    return result
+
+
+@pytest.fixture
+def structure_list_nifti_fixture(
+        mask_list_fixture,
+        tmp_path_factory):
+    """
+    Returns list of configs like
+        "structure": structure name
+        "path": path to NIFTI file
+    """
+    tmp_dir = pathlib.Path(
+        tmp_path_factory.mktemp("list_of_nifti"))
+
+    config_list = []
+    for mask_config in mask_list_fixture:
+        this_config = dict()
+        this_config["structure"] = mask_config["structure"]
+        nifti_path = mkstemp_clean(dir=tmp_dir, suffix='.nii')
+        img = SimpleITK.GetImageFromArray(mask_config["mask"])
+        SimpleITK.WriteImage(
+            image=img,
+            fileName=nifti_path)
+        this_config["path"] = str(nifti_path)
+        config_list.append(this_config)
+
+    yield config_list
+
+    _clean_up(tmp_dir)
+
+
+@pytest.mark.parametrize(
+    "n_processors", [3, 2])
+def test_get_mask_lookup(
+        mask_list_fixture,
+        structure_list_nifti_fixture,
+        n_processors):
+
+    actual = get_mask_lookup(
+        nifti_config_list=structure_list_nifti_fixture,
+        n_processors=n_processors)
+
+    assert len(actual) == len(mask_list_fixture)
+
+    for expected_mask, expected_config in zip(mask_list_fixture,
+                                              structure_list_nifti_fixture):
+        expected_result = get_mask_from_NIFTI(
+            nifti_path=expected_config['path'])
+        this_actual = actual[expected_config['structure']]
+        assert expected_config['path'] == this_actual['path']
+        assert this_actual['shape'] == expected_mask['mask'].shape
+        assert len(this_actual['pixels']) == 3
+        for ii in range(3):
+            np.testing.assert_array_equal(
+                this_actual['pixels'][ii],
+                expected_result['pixels'][ii])
